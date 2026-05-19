2bda9ac71b9f7c58853f4f09526b5e91f274d74d
FERDOUS HASAN RATUL <ferdoushasan387@gmail.com>
2026-05-19 21:28:09 +0600

    Add files via upload
    Index and routes

--- routes.php
+++ routes.php
@@ -6,11 +6,11 @@ $router->get("/", function () {
 });
 
 require APP_ROOT . "/auth/controllers/AuthController.php";
-// require APP_ROOT . "/student/controllers/StudentController.php";
+require APP_ROOT . "/student/controllers/StudentController.php";
 require APP_ROOT . "/TA/controllers/TAController.php";
 
 $authController = new AuthController($conn);
-//$studentController = new StudentController($conn);
+$studentController = new StudentController($conn);
 $taController = new TAController($conn);
 
 $router->get("/auth/login", [$authController, "showLogin"]);
@@ -23,30 +23,30 @@ $router->get("/auth/change-password", [$authController, "showChangePassword"]);
 $router->post("/auth/change-password", [$authController, "changePassword"]);
 $router->get("/auth/logout", [$authController, "logout"]);
 
-// $router->get("/student/dashboard", [$studentController, "dashboard"]);
-// $router->get("/student/courses", [$studentController, "courses"]);
-// $router->get("/student/course-details", [$studentController, "courseDetails"]);
-// $router->get("/student/enroll-course", [$studentController, "enrollCourse"]);
-// $router->get("/student/drop-course", [$studentController, "dropCourse"]);
-// $router->get("/student/attempt-history", [$studentController, "attemptHistory"]);
-// $router->get("/student/quiz-result", [$studentController, "quizResult"]);
-// $router->get("/student/take-quiz", [$studentController, "takeQuiz"]);
-// $router->post("/student/take-quiz", [$studentController, "takeQuiz"]);
-// $router->get("/student/leaderboard", [$studentController, "leaderboard"]);
-// $router->get("/student/performance", [$studentController, "performance"]);
-// $router->get("/student/qa-board", [$studentController, "qaBoard"]);
-// $router->post("/student/qa-board", [$studentController, "qaBoard"]);
-// $router->get("/student/view-answers", [$studentController, "viewAnswers"]);
-// $router->get("/student/doubt-sessions", [$studentController, "doubtSessions"]);
-// $router->get("/student/book-doubt-session", [$studentController, "bookDoubtSession"]);
-// $router->post("/student/book-doubt-session", [$studentController, "bookDoubtSession"]);
-// $router->get("/student/profile", [$studentController, "profile"]);
-// $router->post("/student/profile", [$studentController, "profile"]);
-// $router->get("/student/materials", [$studentController, "materials"]);
-// $router->get("/student/my-courses", [$studentController, "myCourses"]);
-// $router->get("/student/announcements", [$studentController, "announcements"]);
-// $router->get("/student/ask-question", [$studentController, "askQuestion"]);
-// $router->get("/student/booked-sessions", [$studentController, "bookedSessions"]);
+$router->get("/student/dashboard", [$studentController, "dashboard"]);
+$router->get("/student/courses", [$studentController, "courses"]);
+$router->get("/student/course-details", [$studentController, "courseDetails"]);
+$router->get("/student/enroll-course", [$studentController, "enrollCourse"]);
+$router->get("/student/drop-course", [$studentController, "dropCourse"]);
+$router->get("/student/attempt-history", [$studentController, "attemptHistory"]);
+$router->get("/student/quiz-result", [$studentController, "quizResult"]);
+$router->get("/student/take-quiz", [$studentController, "takeQuiz"]);
+$router->post("/student/take-quiz", [$studentController, "takeQuiz"]);
+$router->get("/student/leaderboard", [$studentController, "leaderboard"]);
+$router->get("/student/performance", [$studentController, "performance"]);
+$router->get("/student/qa-board", [$studentController, "qaBoard"]);
+$router->post("/student/qa-board", [$studentController, "qaBoard"]);
+$router->get("/student/view-answers", [$studentController, "viewAnswers"]);
+$router->get("/student/doubt-sessions", [$studentController, "doubtSessions"]);
+$router->get("/student/book-doubt-session", [$studentController, "bookDoubtSession"]);
+$router->post("/student/book-doubt-session", [$studentController, "bookDoubtSession"]);
+$router->get("/student/profile", [$studentController, "profile"]);
+$router->post("/student/profile", [$studentController, "profile"]);
+$router->get("/student/materials", [$studentController, "materials"]);
+$router->get("/student/my-courses", [$studentController, "myCourses"]);
+$router->get("/student/announcements", [$studentController, "announcements"]);
+$router->get("/student/ask-question", [$studentController, "askQuestion"]);
+$router->get("/student/booked-sessions", [$studentController, "bookedSessions"]);
 
 $router->get("/ta/dashboard", [$taController, "dashboard"]);
 $router->get("/ta/assigned-courses", [$taController, "assignedCourses"]);
